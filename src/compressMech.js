c9bb11ebad6088a5869c98e5ca2eda8e7c566844
Uni-Parse <theUniParse@gmail.com>
2026-02-16 22:43:42 +0100

    refactore compressMech() to opt azp content

--- src/compressMech.js
+++ src/compressMech.js
@@ -7,20 +7,24 @@ const {
   copyFile,
 } = require('./utilities')
 
-module.exports = {
-  compressMech,
-}
+module.exports = { compressMech }
 
 async function compressMech(
   CORES_LIMIT,
   IO_LIMIT,
-  srcPath,
-  destPath,
+  baseSrcDir,
+  baseDestDir,
   floatDecimal
 ) {
-  const allPaths = await getAllFilePaths(srcPath)
-  const { actPaths, attPaths, infPaths, otherPaths } =
-    filterActPaths(allPaths)
+  const allPaths = await getAllFilePaths(baseSrcDir)
+  const {
+    actPaths,
+    attPaths,
+    infPaths,
+    hiPaths,
+    descrPaths,
+    otherPaths,
+  } = filterActPaths(allPaths)
 
   // copy other files
   await parallelProccess(
@@ -28,24 +32,34 @@ async function compressMech(
     otherPaths,
     IO_LIMIT,
     async p => {
-      const outPath = p.replace(srcPath, destPath)
+      const outPath = p.replace(baseSrcDir, baseDestDir)
       await copyFile(p, outPath)
+      return outPath
     }
   )
 
-  // opt att & inf files
-  const optedInfoConfigs = await parallelProccess(
-    'Opt INF and ATT files',
-    [...infPaths, ...attPaths],
+  // opt mech files: .act .att .inf | .hi .descr .lod# .act.#
+  const results = await parallelProccess(
+    'Opt Mech files',
+    [
+      ...actPaths,
+      ...attPaths,
+      ...infPaths,
+      ...hiPaths,
+      ...descrPaths,
+    ],
     CORES_LIMIT,
     async p => {
-      const content = await fs.readFile(p, 'utf8')
-      const optContent = opt_Att_Inf_Content(
-        content,
-        floatDecimal
-      )
+      const outPath = p.replace(baseSrcDir, baseDestDir)
 
-      const outPath = p.replace(srcPath, destPath)
+      const content = await fs.readFile(p, 'utf8')
+
+      let optContent = content
+      if ([...attPaths, ...infPaths].includes(p))
+        optContent = opt_ATT_INF_content(content, floatDecimal)
+      else if ([...hiPaths, ...descrPaths].includes(p))
+        optContent = opt_HI_DESCR_content(content)
+      else optContent = opt_ACT_content(content, floatDecimal)
 
       // Create output directory if needed
       const dir = path.dirname(outPath)
@@ -79,63 +93,14 @@ async function compressMech(
     }
   )
 
-  const optedACTs = await parallelProccess(
-    'Opt .Act files',
-    actPaths,
-    CORES_LIMIT,
-    async p => {
-      const content = await fs.readFile(p, 'utf8')
-      const optContent = opt_Act_Content(content, floatDecimal)
-
-      const outPath = p.replace(srcPath, destPath)
-
-      // Create output directory if needed
-      const dir = path.dirname(outPath)
-      await fs.mkdir(dir, { recursive: true })
-
-      await fs.writeFile(outPath, optContent, 'utf8')
-
-      const filename = path.basename(p)
-      const orgSize = content.length
-      const optSize = optContent.length
-      const savedSize = orgSize - optSize
-      const savedPercent = (
-        (savedSize * 100) /
-        orgSize
-      ).toFixed(2)
-
-      // console.log(
-      //   `${filename}: ${sizeToStr(orgSize)} → ${sizeToStr(
-      //     optSize
-      //   )} ` +
-      //     `(Saved ${savedPercent}% ${sizeToStr(savedSize)})`
-      // )
-
-      return {
-        filename,
-        orgSize,
-        optSize,
-        savedSize,
-        savedPercent,
-      }
-    }
-  )
-
-  const configFiles = [...optedACTs, ...optedInfoConfigs]
-  const orgSize = configFiles.reduce(
-    (_, file) => _ + file.orgSize,
-    0
-  )
-
-  const optSize = configFiles.reduce(
-    (_, file) => _ + file.optSize,
-    0
-  )
+  const orgSize = results.reduce((_, r) => _ + r.orgSize, 0)
+  const optSize = results.reduce((_, r) => _ + r.optSize, 0)
   const savedSize = orgSize - optSize
   const savedPercent = Math.round((savedSize / orgSize) * 100)
 
   console.log(
     `\n${'═'.repeat(60)}\n` +
+      `  3d Mech optimization Summary\n` +
       `📊 Total Size: ${sizeToStr(orgSize)} → ${sizeToStr(
         optSize
       )}\n` +
@@ -144,49 +109,48 @@ async function compressMech(
       )}\n` +
       `${'═'.repeat(60)}\n`
   )
+
+  return { orgSize, optSize, savedSize, savedPercent }
 }
 
 function filterActPaths(allPaths) {
   const actPaths = []
   const infPaths = []
   const attPaths = []
+  const hiPaths = []
+  const descrPaths = []
   const otherPaths = []
 
   for (const p of allPaths) {
     const ext = path.extname(p).toLowerCase()
+    const filename = path.basename(p).toLowerCase()
 
     if (ext === '.inf') infPaths.push(p)
     else if (ext === '.att') attPaths.push(p)
-    else if (ext === '.act') actPaths.push(p)
+    else if (ext === '.hi') hiPaths.push(p)
+    else if (ext === '.descr') descrPaths.push(p)
+    else if (
+      ext === '.act' ||
+      // ext === '.actx' ||
+      // ext === '.x' ||
+      /\.act\.\d+$/.test(filename) || // endsWith .ACT.#
+      /\.lod\d+$/.test(filename) // endsWith .LOD#
+    )
+      actPaths.push(p)
     else otherPaths.push(p)
   }
 
-  return { actPaths, infPaths, attPaths, otherPaths }
+  return {
+    actPaths,
+    infPaths,
+    attPaths,
+    hiPaths,
+    descrPaths,
+    otherPaths,
+  }
 }
 
-function opt_Att_Inf_Content(content, floatDecimal) {
-  return (
-    content
-      // Remove /* ... */ comments
-      .replace(/\/\*[\s\S]*?\*\//g, '')
-      // Remove // comments
-      .replace(/\/\/.*$/gm, '')
-      // Optimize floats
-      .replace(/(-?\d*\.\d+)/g, m =>
-        roundFloat(m, floatDecimal)
-      )
-      // split to lines arr
-      .split(/\r?\n/)
-      // trim spaces at the start and end
-      .map(line => line.trim())
-      // remove empty lines
-      .filter(line => line.length > 0)
-      // rejoin lines arr to str
-      .join('\n')
-  )
-}
-
-function opt_Act_Content(content, floatDecimal) {
+function opt_ACT_content(content, floatDecimal) {
   const lines = content
     // Remove /* ... */ comments
     .replace(/\/\*[\s\S]*?\*\//g, '')
@@ -216,6 +180,46 @@ function opt_Act_Content(content, floatDecimal) {
 
   const optContent = `${header}\n${rest}`
   return optContent
+}
+
+function opt_ATT_INF_content(content, floatDecimal) {
+  return (
+    content
+      // Remove /* ... */ comments
+      .replace(/\/\*[\s\S]*?\*\//g, '')
+      // Remove // comments
+      .replace(/\/\/.*$/gm, '')
+      // Optimize floats
+      .replace(/(-?\d*\.\d+)/g, m =>
+        roundFloat(m, floatDecimal)
+      )
+      // split to lines arr
+      .split(/\r?\n/)
+      // trim spaces at the start and end
+      .map(line => line.trim())
+      // remove empty lines
+      .filter(line => line.length > 0)
+      // rejoin lines arr to str
+      .join('\n')
+  )
+}
+
+function opt_HI_DESCR_content(content) {
+  return (
+    content
+      // Remove /* comments */
+      .replace(/\/\*[\s\S]*?\*\//g, '')
+      // Remove // comments
+      .replace(/\/\/.*$/gm, '')
+      // split to lines arr
+      .split(/\r?\n/)
+      // trim extra spaces
+      .map(line => line.trim().replace(/\s{2,}/g, ' '))
+      // remove empty lines
+      .filter(line => line.length > 0)
+      // rejoin lines arr to str
+      .join('\n')
+  )
 }
 
 function roundFloat(float, maxDecimal = 3) {
