74a0960f6eb4f4e01a60569bbb11e8dd07a9fd55
Uni-Parse <theUniParse@gmail.com>
2026-02-20 08:26:55 +0100

    opt mesh utilities

--- src/compressMesh.js
+++ src/compressMesh.js
@@ -9,6 +9,8 @@ const {
 
 module.exports = { compressMesh }
 
+const SHOW_MORE_LOGS = false
+
 async function compressMesh(
   CORES_LIMIT,
   IO_LIMIT,
@@ -17,14 +19,26 @@ async function compressMesh(
   floatDecimal
 ) {
   const allPaths = await getAllFilePaths(baseSrcDir)
-  const {
-    actPaths,
-    attPaths,
-    infPaths,
-    hiPaths,
-    descrPaths,
-    otherPaths,
-  } = filterActPaths(allPaths)
+  const meshPaths = []
+  const configPaths = []
+  const otherPaths = []
+
+  for (const p of allPaths) {
+    const ext = path.extname(p).toLowerCase()
+    const filename = path.basename(p).toLowerCase()
+
+    const isConfig = ['.inf', '.att', '.descr', '.hi'].includes(
+      ext
+    )
+    const isMesh =
+      ['.act', '.actx', '.x'].includes(ext) ||
+      /\.act\.\d+$/.test(filename) || // endsWith .ACT.#
+      /\.lod\d+$/.test(filename) // endsWith .LOD#
+
+    if (isConfig) configPaths.push(p)
+    else if (isMesh) meshPaths.push(p)
+    else otherPaths.push(p)
+  }
 
   // copy other files
   await parallelProccess(
@@ -38,41 +52,51 @@ async function compressMesh(
     }
   )
 
-  // opt mesh files: .act .att .inf | .hi .descr .lod# .act.#
-  const results = await parallelProccess(
-    'Opt Mesh files',
-    [
-      ...actPaths,
-      ...attPaths,
-      ...infPaths,
-      ...hiPaths,
-      ...descrPaths,
-    ],
+  // opt mesh config files: .inf .att .descr .hi
+  const configResults = await parallelProccess(
+    'Opt Mesh config files',
+    configPaths,
     CORES_LIMIT,
     async p => {
+      const filename = path.basename(p)
+      const outPath = p.replace(baseSrcDir, baseDestDir)
+      const content = await fs.readFile(p, 'utf8')
+
+      const optContent = optMeshConfigContent(content)
+
+      // Create output directory if needed
+      const dir = path.dirname(outPath)
+      await fs.mkdir(dir, { recursive: true })
+
+      await fs.writeFile(outPath, optContent, 'utf8')
+
+      const orgSize = content.length
+      const optSize = optContent.length
+
+      return { filename, orgSize, optSize }
+    }
+  )
+
+  // opt 3d mech files: .x .act .actx .lod# .act.#
+  const meshResults = await parallelProccess(
+    'Opt Mesh files',
+    meshPaths,
+    CORES_LIMIT,
+    async p => {
+      const filename = path.basename(p)
       const outPath = p.replace(baseSrcDir, baseDestDir)
 
       const content = await fs.readFile(p, 'utf8')
 
-      let optContent = content
-      let isTxt = true
+      // const binHeaders = ['xof 0303bin 0032']
+      // const txtHeaders = ['xof 0302txt 0032', 'xof 0303txt 0032']
+      const header = content.slice(0, 16).toLowerCase()
+      const isTxt =
+        header.includes('txt') && !header.includes('bin')
 
-      if (attPaths.includes(p) || infPaths.includes(p))
-        optContent = opt_ATT_INF_content(content, floatDecimal)
-      else if (hiPaths.includes(p) || descrPaths.includes(p))
-        optContent = opt_HI_DESCR_content(content)
-      else {
-        const header = content.slice(0, 16).toLowerCase()
-
-        // const binHeaders = ['xof 0303bin 0032']
-        // const txtHeaders = ['xof 0302txt 0032', 'xof 0303txt 0032']
-
-        isTxt =
-          header.includes('txt') && !header.includes('bin')
-
-        if (isTxt)
-          optContent = opt_ACT_content(content, floatDecimal)
-      }
+      const optContent = isTxt
+        ? optMeshContent(content, floatDecimal)
+        : content
 
       // Create output directory if needed
       const dir = path.dirname(outPath)
@@ -81,32 +105,14 @@ async function compressMesh(
       if (isTxt) await fs.writeFile(outPath, optContent, 'utf8')
       else await copyFile(p, outPath)
 
-      const filename = path.basename(p)
       const orgSize = content.length
       const optSize = optContent.length
-      const savedSize = orgSize - optSize
-      const savedPercent = (
-        (savedSize * 100) /
-        orgSize
-      ).toFixed(2)
 
-      // console.log(
-      //   `${filename}: ${sizeToStr(orgSize)} → ${sizeToStr(
-      //     optSize
-      //   )} ` +
-      //     `(Saved ${savedPercent}% ${sizeToStr(savedSize)})`
-      // )
-
-      return {
-        filename,
-        orgSize,
-        optSize,
-        savedSize,
-        savedPercent,
-      }
+      return { filename, orgSize, optSize }
     }
   )
 
+  const results = [...configResults, ...meshResults]
   const orgSize = results.reduce((_, r) => _ + r.orgSize, 0)
   const optSize = results.reduce((_, r) => _ + r.optSize, 0)
   const savedSize = orgSize - optSize
@@ -127,44 +133,24 @@ async function compressMesh(
   return { orgSize, optSize, savedSize, savedPercent }
 }
 
-function filterActPaths(allPaths) {
-  const actPaths = []
-  const infPaths = []
-  const attPaths = []
-  const hiPaths = []
-  const descrPaths = []
-  const otherPaths = []
-
-  for (const p of allPaths) {
-    const ext = path.extname(p).toLowerCase()
-    const filename = path.basename(p).toLowerCase()
-
-    if (ext === '.inf') infPaths.push(p)
-    else if (ext === '.att') attPaths.push(p)
-    else if (ext === '.hi') hiPaths.push(p)
-    else if (ext === '.descr') descrPaths.push(p)
-    else if (
-      // ext === '.x' ||
-      // ext === '.actx' ||
-      ext === '.act' ||
-      /\.act\.\d+$/.test(filename) || // endsWith .ACT.#
-      /\.lod\d+$/.test(filename) // endsWith .LOD#
-    )
-      actPaths.push(p)
-    else otherPaths.push(p)
-  }
-
-  return {
-    actPaths,
-    infPaths,
-    attPaths,
-    hiPaths,
-    descrPaths,
-    otherPaths,
-  }
+function optMeshConfigContent(content) {
+  return (
+    content
+      .replace(/\/\/.*$/gm, '') // Remove // comments
+      .replace(/\/\*[\s\S]*?\*\//g, '') // Remove /* comments */
+      // Optimize floats
+      .replace(/(-?\d+\.\d+)/g, matchFloat => {
+        const num = parseFloat(matchFloat).toString()
+        return num.includes('.') ? num : `${num}.0`
+      })
+      .split(/\r?\n/)
+      .map(line => line.trim().replace(/\s{2,}/g, ' ')) // trim extra spaces
+      .filter(line => line.length > 0) // remove empty lines
+      .join('\n')
+  )
 }
 
-function opt_ACT_content(content, floatDecimal) {
+function optMeshContent(content, floatDecimal) {
   const header = content.slice(0, 16)
   const body = content
     .slice(16)
@@ -190,36 +176,7 @@ function opt_ACT_content(content, floatDecimal) {
   return `${header}\n${body}`
 }
 
-function opt_ATT_INF_content(content, floatDecimal) {
-  return (
-    content
-      .replace(/\/\*[\s\S]*?\*\//g, '') // Remove /* comments */
-      .replace(/\/\/.*$/gm, '') // Remove // comments
-      // Optimize floats
-      .replace(/(-?\d+\.\d+)/g, match =>
-        roundFloat(match, floatDecimal)
-      )
-      .split(/\r?\n/) // split to lines arr
-      .map(line => line.trim()) // trim spaces at the edges
-      .filter(line => line.length > 0) // remove empty lines
-      .join('\n') // rejoin lines arr to str
-  )
-}
-
-function opt_HI_DESCR_content(content) {
-  return (
-    content
-      .replace(/\/\*[\s\S]*?\*\//g, '') // Remove /* comments */
-      .replace(/\/\/.*$/gm, '') // Remove // comments
-      .split(/\r?\n/) // split to lines arr
-      // trim extra spaces
-      .map(line => line.trim().replace(/\s{2,}/g, ' '))
-      .filter(line => line.length > 0) // remove empty lines
-      .join('\n') // rejoin lines arr to str
-  )
-}
-
-function roundFloat(float, maxDecimal = 2) {
+function roundFloat(float, maxDecimal = 4) {
   const num = parseFloat(float)
   const rounded =
     Math.round(num * 10 ** maxDecimal) / 10 ** maxDecimal
